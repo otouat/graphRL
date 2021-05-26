996f8482a829dab2f254d1376175f2954f468731
otouat <ousmane.touat@insa-lyon.fr>
2021-05-26 12:26:19 +0200

    Erdos training with GRAN experimentations

--- baselineModels/GRAN/utils/data_helper.py
+++ baselineModels/GRAN/utils/data_helper.py
@@ -207,11 +207,11 @@ def create_graphs(graph_type, data_dir='data', noise=10.0, seed=1234):
             node_attributes=False,
             graph_labels=True)
     elif graph_type == 'erdos':
-        for i in range(500):
-            graphs.append(nx.erdos_renyi_graph(random.randint(num_vertices - 50, num_vertices), p_erdos))
-    elif graph_type == 'watts':
-        for i in range(500):
-            graphs.append(nx.watts_strogatz_graph(num_vertices, k_watts, p_watts))
+        for i in range(100,200):
+            graphs.append(nx.erdos_renyi_graph(i, 0.1))
+    # elif graph_type == 'watts':
+    #     for i in range(500):
+    #         graphs.append(nx.watts_strogatz_graph(num_vertices, k_watts, p_watts))
     elif graph_type == 'barabasi':
         for i in range(500):
             graphs.append(nx.barabasi_albert_graph(random.randint(100, 200), 4))
